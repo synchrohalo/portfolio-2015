fd2df4e1213e6905e3f9dc82641a8811db8ff42e
Joanne Arboleda <jbarboleda@ucdavis.edu>
2015-10-14 19:00:06 -0700

    temp navigation fixed header for better mobile UX

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
 $( document ).ready(function() {
-
-	/********** INDEX **********/
 	function navGoTo( id ){
 		$( "#" + id + "-nav" ).unbind( "click" ).click(function(){
 			$( "html, body" ).animate({ scrollTop: 0 }, "fast", function() {
 				$( "#main" ).animate({
 					bottom:"+=100vh"
-				}, 750, function() {
+				}, 1000, function() {
 					$( "#main" ).css( "display", "none" );
 					//window.location.href = "../Portfolio 2015/html/" + id + ".html"; // testing locally
 					window.location.href = "../html/" + id + ".html"; // when pushing to GitHub
@@ -19,12 +17,24 @@ $( document ).ready(function() {
 	navGoTo( "about" );
 	navGoTo( "contact" );
 	
-	$( "#home-nav" ).unbind( "click" ).click(function(){
+	/*$( "#home-nav" ).unbind( "click" ).click(function(){
 		$( "#main" ).animate({
 			bottom:"-=100vh"
 		}, 750, function() {
 			window.location.href = "../Portfolio 2015/html/" + id + ".html";
 			//window.location.href = "../html/" + id + ".html";
 		});
-	});
+	});*/
+	
+	var isMobile = function(){
+		if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+			return true;
+		}
+		else{ return false; }
+	}
+	
+	if( isMobile() ){
+		$( ".page-nav" ).css( "min-height", "9vh" );
+		$( "#nav.page-nav li p" ).css( "line-height", "9vh" );
+	}
 });
